7be17666d3ad3e27c219008be4932fc83d7b5693
Bossadi Zenith <135658967+code-env@users.noreply.github.com>
2024-03-22 09:25:04 +0000

    feat: changing the content of the site

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverActions: {
+          allowedOrigins: ["https://stunning-space-couscous-xj79jvw4559f6jr-3000.app.github.dev/", "localhost:3000"]
+        }
+      }
+};
 
 export default nextConfig;
