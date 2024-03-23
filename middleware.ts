f16fdb813432792b45fbeea5a1b20eb14625f4ec
Bossadi Zenith <135658967+code-env@users.noreply.github.com>
2024-03-23 13:18:26 +0000

    fixed: uploadthing error

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,4 @@
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
 
 export default authMiddleware({
   afterAuth(auth, req, evt) {
@@ -9,7 +8,7 @@ export default authMiddleware({
     }
   },
 
-  publicRoutes: ["/api/uploadthing", "/api/:path*"],
+  publicRoutes: ["/api/uploadthing", "/:path*"],
   ignoredRoutes: ["/api/uploadthing"],
 });
 
