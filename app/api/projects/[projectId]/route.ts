b8bceb799da65da5fcdf4a05d1350e49701380c8
Romaric250 <78319422+Romaric250@users.noreply.github.com>
2024-05-24 17:17:05 +0000

    feat get a project api route done

--- app/api/projects/[projectId]/route.ts
+++ app/api/projects/[projectId]/route.ts
@@ -6,14 +6,14 @@ import { NextRequest, NextResponse } from "next/server";
 export async function PUT(req:Request, {params}:{params:{projectId:string}}){
     try {
         const {projectId} = params
-        const {title, description} = await req.json()
+        const {title, description, userId} = await req.json()
 
         if(!projectId) return new NextResponse("Project not found", {status:400})
         
         const UpdateProject = await db.project.update({
             where:{
                 id:projectId,
-                userId:"2b0b3b26-85d1-486f-b1b1-52218cafd171"
+                userId:userId
             },
            data:{
             title:title,
@@ -25,6 +25,30 @@ export async function PUT(req:Request, {params}:{params:{projectId:string}}){
         if(!UpdateProject) return new NextResponse("Something happened while updating projce", {status:403})
         return new NextResponse("Project update sucessfully", {status:200})
 
+    } catch (error:any) {
+
+        console.error(error.message)
+        return new NextResponse("Internal server error", {status:500})
+        
+    }
+}
+
+
+export async function GET(req:Request, {params}: {params:{projectId:string}}){
+    try {
+    const {projectId} = params
+    if(!projectId) return new NextResponse("ProjectId not found. please try again later")
+
+    const GetProject = await db.project.findUnique({
+        where:{
+        id:projectId
+    }})
+
+    if(!GetProject) return new NextResponse("Project not found", {status:400})
+    
+    return new NextResponse(JSON.stringify(GetProject),{status:200})
+
+        
     } catch (error:any) {
 
         console.error(error.message)
