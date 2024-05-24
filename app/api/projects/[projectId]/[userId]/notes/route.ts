74939f115c129efd5f9513a84e5402a9084abd3b
Romaric250 <78319422+Romaric250@users.noreply.github.com>
2024-05-24 17:34:43 +0000

    feat create notes api route completed

---
+++ app/api/projects/[projectId]/[userId]/notes/route.ts
@@ -0,0 +1,33 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+
+export async function POST(req:Request, {params}:{params:{projectId:string, userId:string}}){
+    try {
+        
+        const {projectId, userId,} = params
+
+        const {content,title, description} = await req.json()
+
+        const CreateNote = await db.note.create({
+            data:{
+                content,
+                description,
+                title,
+                userId:userId,
+                projectId:projectId
+                
+            }
+        })
+
+        if(!CreateNote) return new NextResponse("An error un expected error occured while creating Note", {status:402})
+        
+        return new NextResponse(JSON.stringify(CreateNote), {status:200})
+        
+    } catch (error:any) {
+
+        console.error(error.messag)
+        return new NextResponse("Internal Server error", {status:500})
+        
+    }
+}
