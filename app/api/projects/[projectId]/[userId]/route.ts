e232141569ff77341e6ef37860871bebdd707ecb
Romaric250 <78319422+Romaric250@users.noreply.github.com>
2024-05-24 02:12:27 +0000

    feat optimised the api

---
+++ app/api/projects/[projectId]/[userId]/route.ts
@@ -0,0 +1,60 @@
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+
+
+export async function PUT(req:Request, {params}:{params:{projectId:string, userId:string}}){
+    try {
+        const {projectId, userId} = params
+        const {title, description} = await req.json()
+
+        if(!projectId) return new NextResponse("Project not found", {status:400})
+        
+        const UpdateProject = await db.project.update({
+            where:{
+                id:projectId,
+                userId:userId
+            },
+           data:{
+            title:title,
+            description:description,
+
+           } 
+        })
+
+        if(!UpdateProject) return new NextResponse("Something happened while updating projce", {status:403})
+        return new NextResponse("Project update sucessfully", {status:200})
+
+    } catch (error:any) {
+
+        console.error(error.message)
+        return new NextResponse("Internal server error", {status:500})
+        
+    }
+}
+
+export async function GET(req:Request, {params}:{params:{projectId:string, userId: string}}){
+    try {
+        const {projectId, userId} = params
+
+        if(!projectId || !projectId) return new NextRequest("project id required")
+
+        const deleteproject = await db.project.delete({
+            where:{
+                id:projectId
+            }
+        })
+
+        if(!deleteproject) return new NextResponse("An error occured while deleting project", {status:400} )
+
+        
+        
+        
+    } catch (error:any) {
+
+        console.error(error.message)
+        return new NextResponse("Internal Server Error", {status:500})
+        
+    }
+
+}
