ae27d6ab76c7cbaf8fd4a97e194216555a98ec03
Romaric250 <78319422+Romaric250@users.noreply.github.com>
2024-05-24 01:15:29 +0000

    feat tested the api and it works

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -6,26 +6,26 @@ export async function POST(req:Request){
 
     try{
 
-        const {title,description} = await req.json()
+        const {title,description, userId} = await req.json()
 
-        if (!title || !description){
+        if (!title || !description || !userId){
             return new NextResponse("title and description are both require", {status:400})
         }
 
-        const User = await currentUser()
-        if(!User){
-            return new NextResponse("Un Authorized", {status:401})
-        }
+        // const User = await currentUser()
+        // if(!User){
+        //     return new NextResponse("UnAuthorized", {status:401})
+        // }
         const user = await db.user.findUnique({
             where:{
-                userId:User.id
+                id:userId
             }
         })
         if (!user){
             return new NextResponse("Sorry user not found", {status:404})
         }
 
-        const Note = await db.project.create({
+        const Project = await db.project.create({
             data:{
                 title,
                 description,
@@ -33,10 +33,10 @@ export async function POST(req:Request){
             }
         })
 
-        if(!Note){
+        if(!Project){
             return new NextResponse("Something happened while creating note...",{status:402})
         }
-        return new NextResponse("Note create succesffuly", {status:201})
+        return new NextResponse(JSON.stringify(Project), {status:201})
 
     }catch(error:any){
         console.error(error.message)
