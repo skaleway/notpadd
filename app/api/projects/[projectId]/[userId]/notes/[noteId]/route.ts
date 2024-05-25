f8f8622379f677ce50cfc64dde7efd3cbbb1cbf4
Romaric250 <78319422+Romaric250@users.noreply.github.com>
2024-05-25 04:20:41 +0000

    all notes api done and dusted

--- app/api/projects/[projectId]/[userId]/notes/[noteId]/route.ts
+++ app/api/projects/[projectId]/[userId]/notes/[noteId]/route.ts
@@ -31,4 +31,78 @@ export async function GET(req:Request, {params}:{params:{projectId:string, userI
 
 export async function PUT(req:Request,{params}:{params:{projectId:string, userId:string, noteId:string}}){
     
+    try {
+        
+        const {projectId,userId, noteId} = params
+
+        const {title, content,description, type,isPublic} = await req.json()
+
+        if (!projectId && !userId && !noteId) return new NextResponse("ProjectId, userId and noteId are required", {status:401})
+
+        const DoesNoteExist = await db.note.findUnique({
+            where:{
+                id:noteId,
+                userId:userId,
+                projectId:projectId
+            }
+        })
+
+        if(!DoesNoteExist) return new NextResponse("Note not found", {status:402})
+
+
+        const updateNote = await db.note.update(
+            
+            {
+                where:{
+                    id:noteId,
+                    userId:userId,
+                    projectId:projectId
+                },
+                data:{
+                    title:title,
+                    content:content,
+                    description:description,
+                    type:type,
+                    isPublic:isPublic
+                }
+
+        })
+
+        if(!updateNote) return new NextResponse("An error occured whilr updating note", {status:400})
+
+        return new NextResponse(JSON.stringify(updateNote), {status:200})
+
+    } catch (error:any) {
+        console.error(error.message)
+        return new NextResponse("Internal server error", {status:500})
+        
+    }
+}
+
+
+export async function DELETE(req:Request, {params}:{params:{projectId:string,userId:string,noteId:string}}){
+    try {
+
+        const {projectId,userId,noteId} = params 
+        if(!projectId && !userId && !noteId) return new NextResponse("ProjectId, userid and noteid are required", {status:401})
+        
+        const DoesNoteExist = await db.note.findUnique({
+            where:
+            {
+                id:noteId, 
+                userId:userId,
+                projectId:projectId
+            }
+        })
+
+        if(!DoesNoteExist) return new NextResponse("Note Not found...", {status:404})
+        
+    return new NextResponse("Note deleted successfully", {status:200})
+    } catch (error:any) {
+
+        console.error(error.message)
+
+        return new NextResponse("Internal Server error", {status:500})
+        
+    }
 }
