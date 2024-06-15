9131ad363359797f638628e144a46d152b51894d
code-env <org.codeen@gmail.com>
2024-06-15 11:57:35 +0100

    feat: added the light and dark mode on the pricing

--- app/(marketing)/_components/navigation.tsx
+++ app/(marketing)/_components/navigation.tsx
@@ -21,7 +21,7 @@ export const Navbar = () => {
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-fit flex items-center gap-x-2 ">
         <Link
-          className="w-full justify-center flex items-center whitespace-nowrap transition duration-150 ease-in-out font-medium rounded px-4 py-1.5  text-zinc-900 bg-gradient-to-r from-white/80 via-white to-white/80 hover:bg-white group"
+          className="w-full justify-center flex items-center whitespace-nowrap transition duration-150 ease-in-out font-medium rounded px-4 py-1.5 text-zinc-200  dark:text-zinc-900 bg-gradient-to-r dark:from-white/80 from-black/80 via-black to-black/80 dark:via-white dark:to-white/80 hover:bg-zinc-400 group"
           href="/sign-in"
         >
           Log in
