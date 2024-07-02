802aa69114631fad68670cd256fb6c039ee2638e
code-env <org.codeen@gmail.com>
2024-07-02 12:22:08 +0100

    feat: added the project headers to the articles page

--- app/manage/projects/[projectId]/page.tsx
+++ app/manage/projects/[projectId]/page.tsx
@@ -44,8 +44,12 @@ const SingleProject = async ({ params }: { params: { projectId: string } }) => {
 
   const code = `
   // this values should be used wisely
-  NEXT_NOTPADD_USER_ID=${user.id}
-  NEXT_NOTPADD_PROJECT_ID=${params.projectId}`;
+  next_notpadd_userId=${user.id}
+  next_notpadd_projectId=${params.projectId}
+  // Uncomment these lines if you need to send these headers
+  // get_all_articles: "True",
+  // get_only_private_articles: "",
+  get_only_public_articles: "True",`;
 
   return (
     <div className="flex flex-col gap-3">
