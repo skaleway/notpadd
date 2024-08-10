39d6ac68e05eb2ada68f72394e776507f8439238
Romaric Lonfonyuy <78319422+Romaric250@users.noreply.github.com>
2024-08-10 01:33:38 +0000

    configuring webhooks

--- app/api/payment-intent/onsuccess/route.ts
+++ app/api/payment-intent/onsuccess/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 
-export async function POST(req:Request){
+export async function GET(req:Request){
     const data = await req.json
 
     console.log("data here", data)
