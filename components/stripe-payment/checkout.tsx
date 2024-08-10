988724f79c417dd5cb4cffbc72f8042acd7a9dd3
Romaric Lonfonyuy <78319422+Romaric250@users.noreply.github.com>
2024-08-10 00:59:15 +0000

    stripe payment almost done

--- components/stripe-payment/checkout.tsx
+++ components/stripe-payment/checkout.tsx
@@ -16,7 +16,7 @@ const CheckoutPage = ({ amount }: { amount: number }) => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    fetch("/api/create-payment-intent", {
+    fetch("/api/payment-intent", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
