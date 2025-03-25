623dc69d76ed69799df6c5a38953447b657a3057
code-env <org.codeen@gmail.com>
2025-03-25 13:18:43 +0100

    feat: added the output for the notpadd/core and env configurations

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,11 +3,15 @@ import { Button } from "@workspace/ui/components/button";
 
 export default async function Page() {
   const user = await db.user.findFirst();
+
+  if (!user) return <div>No user found</div>;
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World from web</h1>
         <Button size="sm">Button</Button>
+
+        {user?.name}
       </div>
     </div>
   );
