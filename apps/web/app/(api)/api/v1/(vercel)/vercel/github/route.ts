2d15580b601438835a8331d4294c3fb935b0fd94
Skaleway <140396134+skalewayteam@users.noreply.github.com>
2025-04-02 06:28:19 +0000

    feat: completed github push intergration api, vecel should hang with thier apis

--- apps/web/app/(api)/api/v1/(vercel)/vercel/github/route.ts
+++ apps/web/app/(api)/api/v1/(vercel)/vercel/github/route.ts
@@ -1,13 +1,16 @@
 import { NextResponse } from 'next/server';
 import axios from 'axios';
 
-const GITHUB_API_URL = 'https://api.github.com/repos/skaleway/notpadd'; 
+const GITHUB_API_URL = 'https://api.github.com/repos/skaleway/notpadd/contents/notpadd_credentials/do_not_edit_this_file.md';
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;  
 
 
-export default async function POST(req:Request) {
+export  async function POST(req:Request) {
   
     const { title, body } = await req.json();
+
+    // const title = "Hi, from localhost"
+    // const body = "Testing push to notpadd"
 
   if (!title || !body) {
     return new NextResponse("Title and body are required", { status: 400 });
@@ -15,15 +18,17 @@ export default async function POST(req:Request) {
 
   try {
     
-    const filePath = 'credentials/do_not_touch.md';
+    const filePath = 'notpadd_credentials/do_not_edit_this_file.md';
 
     const content = Buffer.from(body).toString('base64');
 
-    const { data: currentFileData } = await axios.get(`${GITHUB_API_URL}/${filePath}`, {
+    const { data: currentFileData } = await axios.get(GITHUB_API_URL, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
+
+    console.log("data here:", currentFileData)
 
     const commitMessage = `Automated commit: ${title || 'New Content Published'}`;
     const commitPayload = {
@@ -33,16 +38,18 @@ export default async function POST(req:Request) {
       branch: 'main',
     };
 
-    await axios.put(`${GITHUB_API_URL}/${filePath}`, commitPayload, {
+    const res = await axios.put(GITHUB_API_URL, commitPayload, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
 
+    // console.log("res here:", res)
+
     return new NextResponse("File updated and pushed to GitHub successfully", { status: 200 });
   } catch (error:any) {
-    console.error('Error committing and pushing to GitHub:', error.message);
-    return new NextResponse("Internal server error", { status: 500 });
+    console.error('Error committing and pushing to GitHub:',error);
+    return new NextResponse("internal server error", { status: 500 });
 }
 
 }
