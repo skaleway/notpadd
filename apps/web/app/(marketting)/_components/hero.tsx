52c0f3de2103d84534080837583de7c549525ec3
code-env <org.codeen@gmail.com>
2025-05-10 22:54:46 +0100

    feat: update Hero component layout; adjust border styles and image scaling for improved presentation

--- apps/web/app/(marketting)/_components/hero.tsx
+++ apps/web/app/(marketting)/_components/hero.tsx
@@ -9,7 +9,7 @@ import water from "@/public/show-case/watercolor-2.webp";
 
 const Hero = () => {
   return (
-    <div className="mx-auto mt-16 py-20 w-full px-4 text-center gap-10 flex flex-col border-y border-border/50 overflow-y-hidden relative">
+    <div className="mx-auto mt-16 py-20 w-full px-4 text-center gap-10 flex flex-col border-b border-border/50 overflow-y-hidden relative">
       <div className="flex flex-col gap-y-5 items-center">
         <h1 className="text-6xl font-bold font-lora text-center">
           Write the content <span className="block">not the code</span>
@@ -23,14 +23,14 @@ const Hero = () => {
           <Button variant="secondary">Get a Demo</Button>
         </div>
       </div>
-      <div className="-mb-24 mt max-w-[55rem] w-full h-[500px] border border-border/50 mx-auto backdrop-blur relative shadow-xl rounded-t-3xl">
+      <div className="-mb-24 mt max-w-5xl w-full h-[500px] border border-border/50 mx-auto backdrop-blur relative shadow-xl rounded-t-3xl">
         <div className="w-full h-full relative overflow-hidden rounded-t-3xl bg-background p-2">
           <div className="size-full relative rounded-t-2xl overflow-hidden border">
             <Image
               src={demoLight}
               alt="Hero"
               fill
-              className="block dark:hidden object-contain w-full scale-[1.01]"
+              className="block dark:hidden"
             />
             <Image
               src={demoDark}
