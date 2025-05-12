43f5097ce3ebb5e6b3475ca85813640048f669b6
code-env <org.codeen@gmail.com>
2025-05-12 13:34:08 +0100

    feat: refactor authentication layout and update sign-up component; improve navigation and styling

--- apps/web/app/(auth)/layout.tsx
+++ apps/web/app/(auth)/layout.tsx
@@ -1,17 +1,40 @@
-import Logo from "@/components/logo";
-import React, { ReactNode } from "react";
+"use client";
 
+import { siteConfig } from "@/lib/site";
+import { Icons } from "@workspace/ui/components/icons";
+import { ArrowLeft } from "lucide-react";
+import { ReactNode } from "react";
+import { useRouter } from "next/navigation";
 interface AuthLayoutProps {
   children: ReactNode;
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
+  const router = useRouter();
   return (
-    <div className="min-h-screen flex flex-col gap-10 items-center justify-center">
-      <div className="flex justify-start w-[355.98px] mx-auto">
-        <Logo />
+    <div className="min-h-screen flex">
+      <div className="flex-[1] border-r border-border/50 flex p-20 justify-center flex-col">
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center gap-2">
+            <Icons.logo className="size-10" />
+            <p className="text-2xl font-bold">{siteConfig.name}</p>
+          </div>
+          <p className="text-lg text-muted-foreground">
+            Write the content not the code.
+          </p>
+        </div>
+        <div className="flex flex-col gap-2"></div>
       </div>
-      {children}
+      <div className="bg-muted/50 flex-[3] flex items-center justify-center relative min-h-screen h-full">
+        <button
+          className="absolute top-10 left-10 w-fit items-center gap-2 border outline-none py-2 px-4 rounded-md text-sm hidden md:flex"
+          onClick={() => router.back()}
+        >
+          <ArrowLeft className="size-4" />
+          <span>Back</span>
+        </button>
+        {children}
+      </div>
     </div>
   );
 };
--- apps/web/components/forms/sign-up.tsx
+++ apps/web/components/forms/sign-up.tsx
@@ -1,155 +1,270 @@
 "use client";
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-import Link from "next/link";
-import { useState } from "react";
-import { useSignUp } from "@clerk/nextjs";
-
+import * as Clerk from "@clerk/elements/common";
+import * as SignUp from "@clerk/elements/sign-up";
 import { Button } from "@workspace/ui/components/button";
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@workspace/ui/components/form";
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@workspace/ui/components/card";
+import { Icons } from "@workspace/ui/components/icons";
 import { Input } from "@workspace/ui/components/input";
-import { registerSchema } from "@/lib/validation";
-import OTP from "@/components/forms/otp";
-import { toast } from "sonner";
-import { OAuthSignUp } from "./oauth-signup";
-import Or from "./or";
-import Heading from "@/components/heading";
+import { Label } from "@workspace/ui/components/label";
+import { cn } from "@workspace/ui/lib/utils";
 
-const Register = () => {
-  const { isLoaded, signUp } = useSignUp();
-  const [verifying, setVerifying] = useState(false);
-  const form = useForm<z.infer<typeof registerSchema>>({
-    resolver: zodResolver(registerSchema),
-  });
-
-  const {
-    formState: { isSubmitting },
-    getValues,
-  } = form;
-
-  async function onSubmit(data: z.infer<typeof registerSchema>) {
-    if (!isLoaded) return;
-
-    const { email, password, username } = data;
-
-    const newName = username.split(" ").join("");
-
-    try {
-      await signUp.create({
-        emailAddress: email,
-        password: password,
-        username: newName,
-      });
-      await signUp.prepareEmailAddressVerification({
-        strategy: "email_code",
-      });
-
-      setVerifying(true);
-    } catch (err: any) {
-      return toast.error(err.errors[0].message);
-    }
-  }
-
-  if (verifying) return <OTP email={getValues().email} />;
-
+export default function SignUpForm() {
   return (
-    <div className="w-full ">
-      <Heading title="Get started" description="Create a new account" isAuth />
-      <OAuthSignUp />
+    <div className="grid w-full grow items-center px-4 sm:justify-center">
+      <SignUp.Root>
+        <Clerk.Loading>
+          {(isGlobalLoading) => (
+            <>
+              <SignUp.Step name="start">
+                <div className="w-full sm:w-96 flex flex-col gap-y-4 ">
+                  <div className="flex flex-col gap-y-2">
+                    <h2 className="text-2xl font-bold font-lora">
+                      Create your free account
+                    </h2>
+                    <p className="text-sm text-muted-foreground">
+                      Connect with your favorite social auth
+                    </p>
+                  </div>
+                  <div className="grid gap-y-4">
+                    <div className="grid grid-cols-2 gap-x-4">
+                      <Clerk.Connection name="github" asChild>
+                        <Button
+                          size="sm"
+                          type="button"
+                          disabled={isGlobalLoading}
+                        >
+                          <Clerk.Loading scope="provider:github">
+                            {(isLoading) =>
+                              isLoading ? (
+                                <Icons.Loading className="size-4 animate-spin" />
+                              ) : (
+                                <>
+                                  <Icons.GitHub className="mr-2 size-4" />
+                                  GitHub
+                                </>
+                              )
+                            }
+                          </Clerk.Loading>
+                        </Button>
+                      </Clerk.Connection>
+                      <Clerk.Connection name="google" asChild>
+                        <Button
+                          size="sm"
+                          type="button"
+                          disabled={isGlobalLoading}
+                        >
+                          <Clerk.Loading scope="provider:google">
+                            {(isLoading) =>
+                              isLoading ? (
+                                <Icons.Loading className="size-4 animate-spin" />
+                              ) : (
+                                <>
+                                  <Icons.Google className="mr-2 size-4" />
+                                  Google
+                                </>
+                              )
+                            }
+                          </Clerk.Loading>
+                        </Button>
+                      </Clerk.Connection>
+                    </div>
+                    <p className="flex items-center gap-x-3 text-sm text-muted-foreground before:h-px before:flex-1 before:bg-border after:h-px after:flex-1 after:bg-border font-lora">
+                      or continue with email
+                    </p>
+                    <Clerk.Field name="emailAddress" className="space-y-2">
+                      <Clerk.Label asChild>
+                        <Label className="font-lora">Email address</Label>
+                      </Clerk.Label>
+                      <Clerk.Input type="email" required asChild>
+                        <Input
+                          className="bg-muted/50 outline-none"
+                          placeholder="example@email.com"
+                        />
+                      </Clerk.Input>
+                      <Clerk.FieldError className="block text-sm text-destructive" />
+                    </Clerk.Field>
+                    <Clerk.Field name="password" className="space-y-2">
+                      <Clerk.Label asChild>
+                        <Label className="font-lora">Password</Label>
+                      </Clerk.Label>
+                      <Clerk.Input type="password" required asChild>
+                        <Input
+                          className="bg-muted/50 outline-none"
+                          placeholder="********"
+                        />
+                      </Clerk.Input>
+                      <Clerk.FieldError className="block text-sm text-destructive" />
+                    </Clerk.Field>
+                  </div>
+                  <div>
+                    <div className="grid w-full gap-y-4">
+                      <SignUp.Captcha className="empty:hidden" />
+                      <SignUp.Action submit asChild>
+                        <Button disabled={isGlobalLoading}>
+                          <Clerk.Loading>
+                            {(isLoading) => {
+                              return isLoading ? (
+                                <Icons.Loading className="size-4 animate-spin" />
+                              ) : (
+                                "Continue"
+                              );
+                            }}
+                          </Clerk.Loading>
+                        </Button>
+                      </SignUp.Action>
 
-      <div className="mb-5" />
-      <Or />
-      <div className="mb-5" />
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="w-full flexcol gap-4"
-        >
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="text-neutral-500">Username</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="bossadizenith"
-                    {...field}
-                    disabled={isSubmitting}
-                    className="disabled:opacity-50 disabled:cursor-not-allowed"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="text-neutral-500">Email</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="me@gmail.com"
-                    className="disabled:opacity-50 disabled:cursor-not-allowed"
-                    {...field}
-                    disabled={isSubmitting}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="text-neutral-500">Password</FormLabel>
-                <FormControl>
-                  <Input
-                    className="disabled:opacity-50 disabled:cursor-not-allowed"
-                    type="password"
-                    {...field}
-                    disabled={isSubmitting}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+                      <Clerk.Link
+                        navigate="sign-in"
+                        className="text-start group"
+                      >
+                        Not new to Notpadd?{" "}
+                        <span className="text-blue-500  group-hover:underline font-lora">
+                          Sign in
+                        </span>
+                      </Clerk.Link>
+                    </div>
+                  </div>
+                </div>
+              </SignUp.Step>
 
-          <Button
-            type="submit"
-            className="disabled:opacity-50 disabled:cursor-not-allowed w-full"
-            disabled={isSubmitting}
-            variant="zbtn"
-          >
-            Create an Account
-          </Button>
-          <div className="mt-4 text-center text-sm text-neutral-500">
-            Already have an account?{" "}
-            <Link
-              href="/sign-in"
-              className="underline dark:text-neutral-100 text-neutral-700"
-            >
-              Sign in
-            </Link>
-          </div>
-        </form>
-      </Form>
+              <SignUp.Step name="continue">
+                <div className="w-full sm:w-96">
+                  <div className="flex flex-col gap-y-2">
+                    <h2 className="text-2xl font-bold font-lora">
+                      Continue registration
+                    </h2>
+                    <p className="text-sm text-muted-foreground">
+                      Create a username to continue
+                    </p>
+                  </div>
+                  <div className="grid gap-y-4">
+                    <Clerk.Field name="username" className="space-y-2">
+                      <Clerk.Label>
+                        <Label className="font-lora">Username</Label>
+                      </Clerk.Label>
+                      <Clerk.Input type="text" required asChild>
+                        <Input className="bg-muted/50 outline-none" />
+                      </Clerk.Input>
+                      <Clerk.FieldError className="block text-sm text-destructive" />
+                    </Clerk.Field>
+                  </div>
+                  <div className="grid w-full gap-y-4">
+                    <SignUp.Action submit asChild>
+                      <Button disabled={isGlobalLoading}>
+                        <Clerk.Loading>
+                          {(isLoading) => {
+                            return isLoading ? (
+                              <Icons.Loading className="size-4 animate-spin" />
+                            ) : (
+                              "Continue"
+                            );
+                          }}
+                        </Clerk.Loading>
+                      </Button>
+                    </SignUp.Action>
+                  </div>
+                </div>
+              </SignUp.Step>
+
+              <SignUp.Step name="verifications">
+                <SignUp.Strategy name="email_code">
+                  <div className="w-full sm:w-96">
+                    <div className="flex flex-col gap-y-2">
+                      <h2 className="text-2xl font-bold font-lora">
+                        Verify your email
+                      </h2>
+                      <p className="text-sm text-muted-foreground">
+                        Use the verification link sent to your email address
+                      </p>
+                    </div>
+                    <div className="grid gap-y-4">
+                      <div className="grid items-center justify-center gap-y-2">
+                        <Clerk.Field name="code" className="space-y-2">
+                          <Clerk.Label className="sr-only">
+                            Email address
+                          </Clerk.Label>
+                          <div className="flex justify-center text-center">
+                            <Clerk.Input
+                              type="otp"
+                              className="flex justify-center has-[:disabled]:opacity-50"
+                              autoSubmit
+                              render={({ value, status }) => {
+                                return (
+                                  <div
+                                    data-status={status}
+                                    className={cn(
+                                      "relative flex size-10 items-center justify-center border-y border-r border-input text-sm transition-all first:rounded-l-md first:border-l last:rounded-r-md",
+                                      {
+                                        "z-10 ring-2 ring-ring ring-offset-background":
+                                          status === "cursor" ||
+                                          status === "selected",
+                                      }
+                                    )}
+                                  >
+                                    {value}
+                                    {status === "cursor" && (
+                                      <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
+                                        <div className="animate-caret-blink h-4 w-px bg-foreground duration-1000" />
+                                      </div>
+                                    )}
+                                  </div>
+                                );
+                              }}
+                            />
+                          </div>
+                          <Clerk.FieldError className="block text-center text-sm text-destructive" />
+                        </Clerk.Field>
+                        <SignUp.Action
+                          asChild
+                          resend
+                          className="text-muted-foreground"
+                          fallback={({ resendableAfter }) => (
+                            <Button variant="link" size="sm" disabled>
+                              Didn&apos;t receive a code? Resend (
+                              <span className="tabular-nums">
+                                {resendableAfter}
+                              </span>
+                              )
+                            </Button>
+                          )}
+                        >
+                          <Button type="button" variant="link" size="sm">
+                            Didn&apos;t receive a code? Resend
+                          </Button>
+                        </SignUp.Action>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="grid w-full gap-y-4">
+                        <SignUp.Action submit asChild>
+                          <Button disabled={isGlobalLoading}>
+                            <Clerk.Loading>
+                              {(isLoading) => {
+                                return isLoading ? (
+                                  <Icons.Loading className="size-4 animate-spin" />
+                                ) : (
+                                  "Continue"
+                                );
+                              }}
+                            </Clerk.Loading>
+                          </Button>
+                        </SignUp.Action>
+                      </div>
+                    </div>
+                  </div>
+                </SignUp.Strategy>
+              </SignUp.Step>
+            </>
+          )}
+        </Clerk.Loading>
+      </SignUp.Root>
     </div>
   );
-};
-
-export default Register;
+}
