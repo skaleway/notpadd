2b14549497a892431eeb6368ef30081b8277d09e
Josias <ndjosiasaurel@gmail.com>
2025-05-16 19:52:00 +0100

    add script to easily startup and run app in dev mode

---
+++ run-dev.sh
@@ -0,0 +1,34 @@
+echo "Checking if .env in root"
+if [ ! -f ".env" ]; then
+    echo ".env file does not exist"
+    exit 1
+fi
+
+echo "Installing global packages"
+pnpm install
+
+echo "[packages/core] Installing packages"
+cd packages/core && pnpm run build
+
+echo "[packages/vite] Installing packages"
+cd ../vite && pnpm run build
+
+echo "Checking if .env in packages/database"
+if [ ! -f "../database/.env" ]; then
+    echo ".env file does not exist in packages/database"
+    exit 1
+fi
+
+echo "Running prisma migrate..."
+cd ../database && pnpm prisma migrate dev
+
+echo "Removing node_modules in apps/web (not sure why but this is necessary)"
+cd ../../apps/web
+rm -rf node_modules 
+
+echo "Installing packages in web again"
+pnpm install
+
+echo "Starting next.js server..."
+pnpm run dev &
+
