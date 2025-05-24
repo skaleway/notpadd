3ec60b6bbb41bb36f5d5882d5317374ad444f4f0
code-env <org.codeen@gmail.com>
2025-05-24 19:03:11 +0100

    feat: add Team Name component to Settings page for team identification

---
+++ apps/web/app/(after-auth)/t/[teamId]/settings/_component/team-name.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@workspace/ui/components/card";
+import { Input } from "@workspace/ui/components/input";
+import { Button } from "@workspace/ui/components/button";
+import { Checkbox } from "@workspace/ui/components/checkbox";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@workspace/ui/components/form";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+const TeamName = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+  return (
+    <Card className="flex flex-col gap-4 p-0">
+      <CardHeader className="p-4">
+        <CardTitle>Team Name</CardTitle>
+        <CardDescription>
+          The name of your team. This is used to identify your team
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </CardContent>
+      <CardFooter className="border-t pt-6">
+        <Button className="max-w-40">Save</Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default TeamName;
--- apps/web/app/(after-auth)/t/[teamId]/settings/page.tsx
+++ apps/web/app/(after-auth)/t/[teamId]/settings/page.tsx
@@ -1,7 +1,48 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@workspace/ui/components/card";
+import { Input } from "@workspace/ui/components/input";
+import { Button } from "@workspace/ui/components/button";
+import { Checkbox } from "@workspace/ui/components/checkbox";
 import React from "react";
+import TeamName from "./_component/team-name";
 
 const SettingsPage = () => {
-  return <div>SettingsPage</div>;
+  return (
+    <div className="grid gap-6">
+      <TeamName />
+      <Card x-chunk="dashboard-04-chunk-2">
+        <CardHeader>
+          <CardTitle>Plugins Directory</CardTitle>
+          <CardDescription>
+            The directory within your project, in which your plugins are
+            located.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="flex flex-col gap-4">
+            <Input placeholder="Project Name" defaultValue="/content/plugins" />
+            <div className="flex items-center space-x-2">
+              <Checkbox id="include" defaultChecked />
+              <label
+                htmlFor="include"
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Allow administrators to change the directory.
+              </label>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="border-t px-6 py-4">
+          <Button className="w-fit">Save</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
-
 export default SettingsPage;
